3624d66a3f273e0d79c1e19c6d89311bb3c0d794
Aditya Jha <aj2236@srmisr.edu.in>
2026-02-23 16:07:53 +0530

    UC9: Recursive Palindrome Checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,37 +1,32 @@
-import java.util.Deque;
-import java.util.LinkedList;
-
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
         String original = "madam";
 
-        Deque<Character> deque = new LinkedList<>();
+        boolean result = isPalindrome(original, 0, original.length() - 1);
 
-        // Insert characters into deque
-        for (int i = 0; i < original.length(); i++) {
-            deque.addLast(original.charAt(i));
-        }
-
-        boolean isPalindrome = true;
-
-        // Compare front and rear
-        while (deque.size() > 1) {
-
-            char first = deque.removeFirst();
-            char last = deque.removeLast();
-
-            if (first != last) {
-                isPalindrome = false;
-                break;
-            }
-        }
-
-        if (isPalindrome) {
+        if (result) {
             System.out.println("The string \"" + original + "\" is a Palindrome.");
         } else {
             System.out.println("The string \"" + original + "\" is NOT a Palindrome.");
         }
+    }
+
+    // Recursive method
+    public static boolean isPalindrome(String str, int start, int end) {
+
+        // Base condition
+        if (start >= end) {
+            return true;
+        }
+
+        // If characters don't match
+        if (str.charAt(start) != str.charAt(end)) {
+            return false;
+        }
+
+        // Recursive call
+        return isPalindrome(str, start + 1, end - 1);
     }
 }
