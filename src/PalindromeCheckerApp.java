09a99e20a8771be874cabcf2f6ee61533a48ccf4
Aditya Jha <aj2236@srmisr.edu.in>
2026-02-23 15:52:20 +0530

    UC1: Correct welcome message module

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,12 @@
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("     PALINDROME CHECKER APP      ");
+        System.out.println("=================================");
+        System.out.println("Welcome to the Palindrome Checker Application.");
+        System.out.println("This application will help you understand");
+        System.out.println("different approaches to check palindromes.");
+    }
+}
